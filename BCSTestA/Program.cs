bc2663ceb6e8549e247003ffbfb0ea28a9d386f0
kevin.geng <kevin.x.geng@gmail.com>
2013-11-21 00:29:34 +0800

    整理了一下代码

--- BCSTestA/Program.cs
+++ BCSTestA/Program.cs
@@ -12,6 +12,12 @@ namespace BCSTestA
         static void Main(string[] args)
         {
             //ATestMethod();
+            IStorageProvider st = StorageFactory.GetDefaultStorageProvider();
+            st.PutFile("/FileDB/4376479.jpg", @"D:\E\Nut\三爱\Ftp Full Backup\Temp\4376479.jpg");
+            Console.WriteLine("Put Done");
+            st.GetFile("/FileDB/4376479.jpg", @"D:\E\Nut\三爱\Ftp Full Backup\Temp\4376479.Download.jpg");
+            Console.WriteLine("Get Done");
+            Console.ReadLine();
         }
 
         private static void ATestMethod()
@@ -54,6 +60,97 @@ namespace BCSTestA
         }
 
     }
+    public class StorageFactory {
+        public static IStorageProvider GetDefaultStorageProvider()
+        {
+            return new BcsStorage();
+        }
+        public static T GetStorageProvider<T>() where T:IStorageProvider,new() {
+            return new T();
+        }
+    }
+    public interface IStorageProvider:IDisposable {
+        void PutFile(string targetPath,string localPath);
+        void PutData(string targetPath, byte[] data);
+        void GetFile(string targetPath,string localPath);
+        byte[] GetData(string targetPath);
+        void DeleteFile(string targetPath);
+        void Config(Dictionary<string,object> cfg);
+    }
+    public class BcsStorage : IStorageProvider {
+        BcsClient bcs = new BcsClient();
+
+        public void PutFile(string targetPath, string localPath)
+        {
+            PutData(targetPath, System.IO.File.ReadAllBytes(localPath));
+        }
+
+        public void PutData(string targetPath, byte[] data)
+        {
+            bcs.PutObject(targetPath, data);
+        }
+
+        public void GetFile(string targetPath, string localPath)
+        {
+            System.IO.File.WriteAllBytes(localPath, GetData(targetPath));
+        }
+
+        public byte[] GetData(string targetPath)
+        {
+            return bcs.GetObject(targetPath);
+        }
+        public void DeleteFile(string targetPath) {
+            bcs.DeleteObject(targetPath);
+        }
+
+        public void Config(Dictionary<string, object> cfg)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            bcs.Dispose();
+        }
+    }
+    public class LocalStorage : IStorageProvider
+    {
+
+        public void PutFile(string targetPath, string localPath)
+        {
+            PutData(targetPath, System.IO.File.ReadAllBytes(localPath));
+        }
+
+        public void PutData(string targetPath, byte[] data)
+        {
+            System.IO.File.WriteAllBytes(targetPath, data);
+        }
+
+        public void GetFile(string targetPath, string localPath)
+        {
+            System.IO.File.WriteAllBytes(localPath, GetData(targetPath));
+        }
+
+        public byte[] GetData(string targetPath)
+        {
+            return System.IO.File.ReadAllBytes(targetPath);
+        }
+
+        public void DeleteFile(string targetPath)
+        {
+            System.IO.File.Delete(targetPath);
+        }
+
+        public void Config(Dictionary<string, object> cfg)
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
     public class BcsClient : IDisposable
     {
         string bucketuri = "http://bcs.duapp.com/site3ai";
@@ -63,26 +160,43 @@ namespace BCSTestA
         string bucket = "site3ai";
         const string METHOD_PUT = "PUT";
         const string METHOD_GET = "GET";
+        const string METHOD_DELETE = "DELETE";
         HMAC hmac = null;
         System.Net.WebClient wc = new System.Net.WebClient();
 
-        public void BcsClient()
+        public  BcsClient()
         {
-            HMAC hmac = HMACSHA1.Create();
+            hmac = HMACSHA1.Create();
             hmac.Key = Encoding.ASCII.GetBytes(secretkey);
         }
 
-        public void PutObject(string BcsPathAndName, string fn)
+        //public void PutObject(string BcsPathAndName, string fn)
+        //{
+        //    var apicallurl = CreateApiUrl(METHOD_PUT, BcsPathAndName);
+        //    var result = wc.UploadFile(apicallurl, METHOD_PUT, fn);
+        //}
+        public void PutObject(string BcsPathAndName, byte[] data)
         {
             var apicallurl = CreateApiUrl(METHOD_PUT, BcsPathAndName);
-            var result = wc.UploadFile(apicallurl, METHOD_PUT, fn);
+            
+            var result = wc.UploadData(apicallurl, METHOD_PUT,data );
         }
+        //public void GetObject(string BcsPathAndName, string fn)
+        //{
+        //    var bs = GetObject(BcsPathAndName);
+        //    System.IO.File.WriteAllBytes(fn,bs);
+        //}
         public byte[] GetObject(string BcsPathAndName)
         {
             var apicallurl = CreateApiUrl(METHOD_GET, BcsPathAndName);
             var result = wc.DownloadData(apicallurl);
             return result;
         }
+        public void DeleteObject(string BcsPathAndName) {
+            var apicallurl = CreateApiUrl(METHOD_DELETE, BcsPathAndName);
+            var result = wc.DownloadData(apicallurl);
+        }
+
         //public void DownloadObject(string BcsPathAndName, string fn)
         //{ 
 
@@ -101,7 +215,7 @@ namespace BCSTestA
 
         private string BcsSignature(string method, string obj)
         {
-            var actionstring = string.Format("{0}\nMethod={1}\nBucket={2}\nObject=/{3}\n", flag, , bucket, obj);
+            var actionstring = string.Format("{0}\nMethod={1}\nBucket={2}\nObject=/{3}\n", flag, method, bucket, obj);
             var hash= hmac.ComputeHash(Encoding.ASCII.GetBytes(actionstring));
             return System.Net.WebUtility.UrlEncode(Convert.ToBase64String(hash));
         }
@@ -111,4 +225,5 @@ namespace BCSTestA
             hmac = null;
         }
     }
+    
 }
